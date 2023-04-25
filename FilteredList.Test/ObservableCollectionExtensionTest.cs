446807dfc51acfe87ccf479f5e49c82290fd9f7b
Francesco Paolocci <micky5991@gmail.com>
2023-04-25 21:38:20 +0200

    Add extensions from observablecollection to filteredlist

---
+++ FilteredList.Test/ObservableCollectionExtensionTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using FluentAssertions;
+using Micky5991.FilteredList.Extensions;
+using Micky5991.FilteredList.Test.TestTypes;
+
+namespace Micky5991.FilteredList.Test;
+
+[TestClass]
+public class ObservableCollectionExtensionTest
+{
+    private ObservableCollection<IFurniture> collection;
+
+    [TestInitialize]
+    public void Init()
+    {
+        this.collection = new ObservableCollection<IFurniture>
+        {
+            new Chair(1),
+            new Chair(5),
+            new Chair(10),
+            new Table(1),
+            new Table(5),
+            new Table(10),
+        };
+    }
+
+    [TestMethod]
+    public void ToFilteredListSimpleShouldCreateFilteredList()
+    {
+        var list = this.collection.ToFilteredList();
+
+        list.Should().BeOfType<FilteredList<ITable>>();
+        list.Source.Should().BeSameAs(this.collection);
+    }
+
+    [TestMethod]
+    public void ToFilteredListNullShouldCreateFilteredList()
+    {
+        var list = this.collection.ToFilteredList(null!);
+
+        list.Should().BeOfType<FilteredList<ITable>>();
+        list.Source.Should().BeSameAs(this.collection);
+    }
+
+    [TestMethod]
+    public void ToFilteredListFilterShouldCreateFilteredList()
+    {
+        var list = this.collection.ToFilteredList(_ => true);
+
+        list.Should().BeOfType<FilteredList<ITable>>();
+        list.Source.Should().BeSameAs(this.collection);
+    }
+
+    [TestMethod]
+    public void ToFilteredListTypeFilterShouldCreateFilteredList()
+    {
+        var list = this.collection.ToFilteredList((ITable _) => true);
+
+        list.Should().BeOfType<FilteredList<ITable, IFurniture>>();
+        list.Source.Should().BeSameAs(this.collection);
+    }
+}
---
+++ FilteredList/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Diagnostics;
+
+namespace Micky5991.FilteredList.Extensions;
+
+public static class ObservableCollectionExtensions
+{
+    public static FilteredList<T> ToFilteredList<T>(this ObservableCollection<T> collection, Predicate<T>? filter = null)
+    {
+        Guard.IsNotNull(collection);
+
+        return new FilteredList<T>(collection, filter);
+    }
+
+    public static FilteredList<TNew, TSource> ToFilteredList<TNew, TSource>(this ObservableCollection<TSource> collection, Predicate<TNew>? filter = null)
+        where TNew : TSource
+    {
+        Guard.IsNotNull(collection);
+
+        return new FilteredList<TNew, TSource>(collection, filter);
+    }
+}
